3e05c96f8c3161182ec79dd3e3462283d83795b1
Binod Suman <binodkumarsuman@gmail.com>
2021-08-20 00:19:37 +0530

    Delete Dependency_Injection_6 directory

--- Dependency_Injection_6/src/main/java/di/LogMessageConfiguration.java
+++
@@ -1,9 +0,0 @@
-package di;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-
-public class LogMessageConfiguration {
-
-}
